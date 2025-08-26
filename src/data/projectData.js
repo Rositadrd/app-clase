5d2f1ec6da2f5bfc4a05afecfa82be6d1d44e1b2
RoseA.González <139820524+Rositadrd@users.noreply.github.com>
2025-08-26 15:58:35 -0400

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,16 +3,16 @@ export const projects = [
   {
     id: 1,
     title: {
-      en: "",
-      es: "",
+      en: "Ciencias Agrícolas, Conocer es Saber",
+      es: "Ciencias Agrícolas, Conocer es Saber",
     },
     description: {
-      en: "",
-      es: "",
+      en: "A website dedicated to the dissemination of agricultural sciences, agriculture, and home gardening. Learn more about agriculture-related professions and read interviews with experts in the field.",
+      es: "Un sitio web dedicado a la divulgación de las ciencias agrícolas, la agricultura y los huertos caseros. Conoce más sobre las profesiones relacionadas con la agricultura y lee entrevistas con expertos en el campo.",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: "https://rositadrd.github.io/Ciencias-Agr-colas/",
+    githubRepo: "https://github.com/Rositadrd/Ciencias-Agr-colas",
   },
   // Add more projects here...
 ];
